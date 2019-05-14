301c74dcd881432f066d631530ca001bb37e1c03
hasan9794 <hasanm9794@gmail.com>
2019-05-14 13:05:54 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import FbImageLibrary from 'react-fb-image-grid';
+import logo from './logo.svg';
+import './App.css';
+import * as moment from 'moment';
+import RecipeReviewCard from './components/card'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function App() {
+
+  const images = [
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvQZAP37ISz6Hh9l-9g71FwE4aYKw0rWpaVOiEtRsjO7c3Fmg-',
+    'https://secure.i.telegraph.co.uk/multimedia/archive/03350/gardening-music_3350010b.jpg',
+    'https://secure.i.telegraph.co.uk/multimedia/archive/03350/gardening-music_3350010b.jpg',
+    
+  ]
+  return (
+    <div className="col-sm">
+      <div>
+          <RecipeReviewCard 
+            profileImage = "https://scontent.fkhi6-1.fna.fbcdn.net/v/t1.0-9/42792829_2481494735224033_889299822182400000_n.jpg?_nc_cat=104&_nc_ht=scontent.fkhi6-1.fna&oh=db279c89093ef863053805388add86e8&oe=5D57C775"
+            title= "Muhammad Hasan"
+            date = "4 Hours Ago"
+            images = {images}
+            shortDescription = "This impressive paella is a perfect party dish and a fun meal to cook together with your
+            guests. Add 1 cup of frozen peas along with the mussels, if you like."
+            
+            description = "Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
+            chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
+            salt and pepper, and cook, stirring often until thickened and fragrant, about 10
+            minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil."
+          />             
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/card.js
@@ -0,0 +1,137 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import FbImageLibrary from 'react-fb-image-grid';
+import FacebookEmoji from 'react-facebook-emoji';
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+});
+
+class RecipeReviewCard extends React.Component {
+  state = { expanded: false, emojis: false };
+
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  };
+
+  showEmojis(){
+      this.setState({emojis: true})
+      
+    }
+    
+    hideEmojis(){
+      this.setState({emojis: false})
+      console.log(this)
+      
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              <img style={{width: 50}} src={this.props.profileImage} />
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title={this.props.title}
+          subheader={this.props.date}
+        />
+        <FbImageLibrary 
+            images={this.props.images}
+        />
+        
+        <CardContent>
+          <Typography component="p">
+            {this.props.shortDescription}
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.actions} disableActionSpacing>
+          
+          <IconButton aria-label="Add to favorites">
+               { this.state.emojis && <div>
+                  <FacebookEmoji type="like"/>
+               </div> 
+              }
+              <div></div>
+              <div>
+               {<button onMouseOver={this.showEmojis.bind(this)} onMouseOut={this.hideEmojis.bind(this)} >React</button>}
+              </div> 
+              {/* <FavoriteIcon /> */}
+          </IconButton>
+          <IconButton aria-label="Share">
+            <ShareIcon />
+          </IconButton>
+          <IconButton
+            className={classnames(classes.expand, {
+              [classes.expandOpen]: this.state.expanded,
+            })}
+            onClick={this.handleExpandClick}
+            aria-expanded={this.state.expanded}
+            aria-label="Show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>
+              {this.props.description}
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
+    );
+  }
+}
+
+RecipeReviewCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(RecipeReviewCard);
